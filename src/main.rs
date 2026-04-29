08931226dc4efa4ff91da00f15925f591e5d60e8
toantran292 <tranthaitoan998@gmail.com>
2026-04-29 10:22:53 +0700

    release: v0.2.16

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod tui;
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 
-const VERSION: &str = "0.2.15";
+const VERSION: &str = "0.2.16";
 
 #[derive(Parser)]
 #[command(name = "tncli", about = "tmux-based project launcher", version = VERSION)]
